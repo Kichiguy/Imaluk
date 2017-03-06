f776254c345eb9dba567969f63d7c319d28efff5
Bobby Brace <sedarmor@gmail.com>
2017-03-05 20:45:38 -0600

    Added some more assets and began script work

---
+++ Unity/Imaluk/Assets/Scripts/BoardTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTile : MonoBehaviour {
+
+    private Board board = new Board();
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
